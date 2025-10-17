87faed2f54434d0543ec84096e56a9a98ca21641
Duc <ngohoaiduc08@gmail.com>
2025-10-17 19:54:55 +0700

    Done: Create Project

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from "express";
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Trang chủ");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port: ${port}`);
+});
