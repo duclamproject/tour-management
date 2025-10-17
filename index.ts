a50901b39a8b0d353794161b2877336fc63ce49a
Duc <ngohoaiduc08@gmail.com>
2025-10-18 01:25:29 +0700

    DONE: USING SQUELIZE CONNECT TO MYSQL

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import sequelize from "./config/database";
+
+dotenv.config();
+
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
 
 app.set("views", `./views`);
 app.set("view engine", "pug");
+
+sequelize;
 
 app.get("/tours", (req: Request, res: Response) => {
   res.render("client/pages/tours/index");
