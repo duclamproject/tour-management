a577b57fbcb55bf226aba970f19167ac387ecad3
Duc <ngohoaiduc08@gmail.com>
2025-10-18 16:15:53 +0700

    DONE: LIST TOURS BY CATEGORIES

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,10 +1,40 @@
 import { Request, Response } from "express";
 import Tour from "../../models/tour.model";
+import sequelize from "../../config/database";
+import { QueryTypes } from "sequelize";
 export const index = async (req: Request, res: Response) => {
-  const tours = await Tour.findAll({
-    where: { deleted: false, status: "active" },
-    raw: true,
+  const slugCategory = req.params.slugCategory;
+  // const tours = await Tour.findAll({
+  //   where: { deleted: false, status: "active" },
+  //   raw: true,
+  // });
+
+  const tours = await sequelize.query(
+    `
+    SELECT tours.*, ROUND(price * (1 - discount / 100)) AS price_special
+    FROM tours
+    JOIN tours_categories ON tours.id = tours_categories.tour_id
+    JOIN categories ON tours_categories.category_id = categories.id
+    WHERE 
+      categories.slug = '${slugCategory}'
+      AND tours.deleted = false
+      AND tours.status = 'active'
+      AND categories.deleted = false
+      AND categories.status = 'active'
+    `,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  tours.forEach((tour: any) => {
+    tour.price_special = parseFloat(tour.price_special);
+    tour.images = JSON.parse(tour.images);
+    tour.image = tour.images[0];
   });
+
+  // console.log(tours);
+
   res.render("client/pages/tours/index", {
     pageTitle: "Danh sách tour",
     tours: tours,
