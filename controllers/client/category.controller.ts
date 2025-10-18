55b47c7f8150ed1fe2fb3f5fe73243c26eac4272
Duc <ngohoaiduc08@gmail.com>
2025-10-18 15:09:21 +0700

    DONE: CATEGORIES

---
+++ controllers/client/category.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import Category from "../../models/category.model";
+export const index = async (req: Request, res: Response) => {
+  const categories = await Category.findAll({
+    where: {
+      deleted: false,
+      status: "active",
+    },
+    raw: true,
+  });
+
+  res.render("client/pages/categories/index", {
+    pageTitle: "Categories",
+    categories: categories,
+  });
+};
--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { tourRouter } from "./tour.route";
+import { categoryRouter } from "./category.route";
 
 const clientRoutes = (app: Express): void => {
   app.use("/tours", tourRouter);
+  app.use("/categories", categoryRouter);
 };
 
 export default clientRoutes;
